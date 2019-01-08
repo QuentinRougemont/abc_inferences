b11fb88e7b98a481c7a55aad769276528f4bbfac
ubuntu <quentinrougemont@orange.fr>
2019-01-08 04:53:41 -0500

    update bash script

--- 00-scripts/00.reshape.sh
+++ 00-scripts/00.reshape.sh
@@ -4,16 +4,38 @@
 #to be use carefully
 target=1000000 #set the number of wanted simulations
 
-ls -d results/*/ | sed -e 's/\([0-9]*\)//g' -e 's/._\///g'  -e 's/results\///g' |uniq > list
+ls -d results/*/ | \
+    sed -e 's/\([0-9]*\)//g' \
+     -e 's/._\///g' -e 's/results\///g' |\
+     uniq > list
 
-for j in $(cat list) ; do mkdir results/$j.glob ; done 
-for j in  $(cat list) ; do mv results/$j.* results/$j.glob ; done 
-for i in $(cat list) ; do for k in $(find results/$i.glob -name ABCstat.txt) ; do cat $k |grep -v dataset >> results/$i.ABC.stat.txt ; done ; done  
-for i in $(cat list) ; do for k in $(find results/$i.glob -name priorfile) ; do cat $k |grep -v N1 >> results/$i.priorfile.txt ; done ; done 
+for i in  $(cat list) ; do 
+    mkdir results/"$i".glob
+    mv results/"$i".h*_* results/"$i".glob ; 
+    for k in $(find results/"$i".glob -name ABCstat.txt) ; do 
+        cat "$k" |grep -v dataset >> results/"$i".ABC.stat.txt ; 
+    done   
+   for k in $(find results/$i.glob -name priorfile) ; do 
+       cat "$k" |grep -v N1 >> results/"$i".priorfile.txt ; 
+   done ; 
+done
+
 sed -i '/^$/d' results/*.h*.ABC.stat.txt 
 
-for j in  *.glob ; do rm results/$j/*/myfifo  results/$j/*/error.txt results/$j/*/sp*txt results/$j/*/bpfile results/$j/*/seedms ; done
-for i in results ; do for j  in  $i/*.glob  ; do  tar -zcvf $j.tar.gz $j ; done ;done
+for j in  *.glob ; do 
+    rm results/$j/*/myfifo \
+       results/$j/*/error.txt \
+       results/$j/*/sp*txt \
+       results/$j/*/bpfile \
+       results/$j/*/seedms ; 
+done
+
+for i in results ; do 
+    for j  in  $i/*.glob  ; do  
+            tar -zcvf $j.tar.gz $j ; 
+        done ;
+done
+
 for j in $(ls -d results/*glob/ ) ; do  rm  -r $j ; done
 #rm *.err *.out
 
