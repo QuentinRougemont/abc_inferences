4cf36164a1fcbda96c881ae6c7252c7341e65d11
quentin <quentinrougemont@orange.fr>
2021-02-15 08:34:33 +0100

    add info

--- 00-scripts/utility_scripts/ped2matrix.R
+++ 00-scripts/utility_scripts/ped2matrix.R
@@ -3,18 +3,17 @@
 
 #Author= "Quentin Rougemont"
 #purpose = "Miniscript to reshape a classical ped file into a genotypic matrix (usefull for several programms)
-#how to run = "03.run.spacemix.R sample.covariance.matrix sample.size x_y.coordinates fast.model.opts long.model.opts nloci 
+#how to run = "Rscrippt ped2matrix.R input.ped input.map" #with input are file obtained from vcftools --plink or plink --vcf
 #last uptade = "15.09.2016"
 #Output = genotypic matrix (AC, TG, AA, CC, etc. with inds in row, mk in cols)
 
 argv <- commandArgs(TRUE) #Ped file
 
 if (argv[1]=="-h" || length(argv)==0){
-	cat("\nped2matrix.R \"input.ped\" \"input.map\" \n" )
+        cat("usage:\nRscript ped2matrix.R \"input.ped\" \"input.map\" \nwith input being plink ped/map file\n" )
 }else{
 
-
-
+#read data
 dat  <-argv[1] 
 loc  <-argv[2]
 dat2 <-as.matrix(read.table(dat,h=F)) 
@@ -26,12 +25,12 @@ dat4  <- read.table(loc)[,2]
 
 dat5  <- as.matrix( t(c("ind","pop",as.character(dat4)) ))
 start <- seq(1, by = 2, length = ncol(dat3) / 2)
-
+#conversion
 sdf   <- sapply(start,function(i, dat3) paste(as.character(dat3[,i]),as.character(dat3[,i+1]), sep="") ,dat3 = dat3) 
 
 sdf[sdf=="NANA"] <- "--"
 sdf[sdf=="TRUE"] <- "T"
-
+#write
 write.table( rbind( dat5, as.matrix( cbind(dat2[,c(1:2)],as.data.frame(sdf)) ) ),"genotypic.matrix",quote=F,col.names=F,row.names=F)
 remove <- paste("sed -i 's/TRUE/T/g' genotypic.matrix ")
 system(remove)
